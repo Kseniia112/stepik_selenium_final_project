5552eb1931dc99f35cc2d6b8a6ea81c5ab916c61
Kseniia112 <kseniya.savotina@gmail.com>
2024-06-30 17:02:59 +0300

    added ProductPageLocators class for Product Page locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,11 +1,11 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-class LoginPageLocators():
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form")
     LOGIN_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_login-username")
     LOGIN_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_login-password")
@@ -16,3 +16,12 @@ class LoginPageLocators():
     REGISTER_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTER_CONFIRMATION_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
     REGISTER_BUTTON = (By.CSS_SELECTOR, "[name=registration_submit]")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main  h1")
+    FIRST_ALERT = (By.CSS_SELECTOR, "div.alertinner")
+    FIRST_ALERT_PRODUCT_NAME = (By.CSS_SELECTOR, "div#messages :first-child div.alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    ALERT_PRODUCT_PRICE = (By.CSS_SELECTOR, "div#messages div.alertinner p strong")
