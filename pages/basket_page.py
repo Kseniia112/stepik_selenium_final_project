e8aaabe4c40050fbcd05fbbf369aa240bdd9de1a
Kseniia112 <kseniya.savotina@gmail.com>
2024-07-04 15:44:34 +0300

    add assertion depending on page language

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,3 +1,5 @@
+from . import languages
+
 from .base_page import BasePage
 from .locators import BasketPageLocators, BasePageLocators
 
@@ -5,8 +7,9 @@ from .locators import BasketPageLocators, BasePageLocators
 class BasketPage(BasePage):
     def should_be_empty(self):
         assert self.is_not_element_present(*BasketPageLocators.PRODUCT_FORM), 'Basket is not empty'
+        lang = self.browser.find_element(*BasePageLocators.LANGUAGE).get_attribute("lang")
         basket_text = self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_TEXT).text
-        assert 'Ваша корзина пуста' in basket_text, 'Message text is not displayed'
+        assert languages.empty_basket[lang] in basket_text, 'Message text is not displayed'
 
     def should_be_product(self):
         pass
